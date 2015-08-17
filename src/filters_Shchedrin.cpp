e23b0bfe76e881ff6bc12203865bef0118d66838
rashchedrin <ss2015@ws-2k-110-n11.ccam.cmc.unn.net>
2015-08-17 18:51:32 +0300

    fixed cols/rows bug

--- src/filters_Shchedrin.cpp
+++ src/filters_Shchedrin.cpp
@@ -3,8 +3,10 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <assert.h>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 class FiltersShchedrin : public Filters
 {
@@ -13,17 +15,18 @@ public:
    { }
    virtual void filter2d(const Matrix &src, Matrix& dst, const Matrix &kernel)
    { 
-        //dst = src; //TODO: remove
+        //dst = src; 
+        //return;
         int cols, rows;
         cols = src.cols();
         rows = src.rows();
         int centerC = kernel.cols()/2;
         int centerR = kernel.rows()/2;
-        for(int i = 0; i < dst.cols(); i++){
-            for(int j = 0; j < dst.rows(); j++){
-                int sum = 0;
-                for(int kc = 0; kc < kernel.cols(); kc++){
-                    for(int kr = 0; kr < kernel.rows(); kr++){
+        for(int i = 0; i < dst.rows(); i++){
+            for(int j = 0; j < dst.cols(); j++){
+                unsigned int sum = 0;
+                for(int kr = 0; kr < kernel.rows(); kr++){
+                     for(int kc = 0; kc < kernel.cols(); kc++){
                         unsigned char srcElem, kerElem;
                         int srcCol = i - centerC + kc;
                         int srcRow = j - centerR + kr;
@@ -31,25 +34,28 @@ public:
                             srcElem = 0;
                         }else{
                             assert(srcCol >= 0 && srcRow >= 0 && srcCol < src.cols() && srcRow < src.rows());
-                            srcElem = src[srcCol][srcRow];
+                            srcElem = src[srcRow][srcCol];
                         }
-                        assert(kc>=0);
-                        assert(true&& kr >= 0);
-                        assert(true&& kc < kernel.cols() );
-                        assert(true&& kr < kernel.rows());
+                        assert(kc >=0);
+                        assert(kr >= 0);
+                        assert(kc < kernel.cols());
+                        assert(kr < kernel.rows());
                         kerElem = kernel[kc][kr];
                         sum += kerElem * srcElem;
                     }
                 }
-                assert(i >= 0 && j >= 0 && i < dst.cols() && j < dst.rows());
+                //cout<<i<<" "<<j<<endl; // TODO:remove
+                assert(i >= 0 && j >= 0 && i < dst.rows() && j < dst.cols());
                 if(sum < 256){
                     dst[i][j] = sum;
                 }
                 else{
                     dst[i][j] = 255;
                 }
+                //cout<<"OK"<<endl; // TODO:remove
             }
         }
+        std::cout<<"end"<<std::endl;
    }
    virtual void median(const Matrix &src, Matrix &dst, const int kSize = 3)
    { }
